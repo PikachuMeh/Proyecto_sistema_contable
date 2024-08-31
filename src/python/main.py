b5fe6eeb83152abe5e75eff8e5f880540b4e1bf9
PikachuMeh <juanes.malave@gmail.com>
2024-08-31 17:15:50 -0400

    eliminar codigo basura

--- src/python/main.py
+++ src/python/main.py
@@ -178,57 +178,6 @@ def buscar_empresas(request: BuscarEmpresaRequest):
         raise HTTPException(status_code=404, detail="No companies found")
     return empresas
 
-
-@app.post("/empresas/{empresa_id}/crear-cuentas-y-plan")
-def crear_cuentas_y_plan(empresa_id: int, cuentas: list[CuentaContableSchema]):
-    try:
-        empresa = session.query(Empresas).filter(Empresas.id_empresas == empresa_id).first()
-        if not empresa:
-            raise HTTPException(status_code=404, detail="Empresa no encontrada")
-
-        # Crear plan de cuentas
-        nuevo_plan = PlanCuentas(
-            codigo="PLAN_" + str(empresa_id),  # Usar un código válido
-            descripcion_cuenta="Plan de cuentas para la empresa " + str(empresa_id),
-            registro_empresas=empresa_id
-        )
-        session.add(nuevo_plan)
-        session.commit()
-
-        for cuenta in cuentas:
-            # Validar la jerarquía de código de cuenta
-            codigo_partes = cuenta.codigo_cuenta.split('.')
-            if len(codigo_partes) > 1:
-                codigo_padre = '.'.join(codigo_partes[:-1])
-                cuenta_padre = session.query(CuentasContables).filter(
-                    CuentasContables.codigo == codigo_padre,
-                    CuentasContables.empresas_id == empresa_id
-                ).first()
-                if not cuenta_padre:
-                    raise HTTPException(status_code=400, detail=f"Debe existir una cuenta padre {codigo_padre} para crear la subcuenta {cuenta.codigo_cuenta}")
-
-            # Crear cuenta contable
-            nueva_cuenta = CuentasContables(
-                codigo=cuenta.codigo_cuenta,
-                descripcion_cuenta=cuenta.descripcion_cuenta,
-                nombre_cuenta=cuenta.nombre_cuenta,
-                nivel_cuenta=cuenta.nivel_cuenta,
-                tipo_cuenta=cuenta.tipo_cuenta,
-                saldo_normal=cuenta.saldo_normal,
-                estado_cuenta="abierto",
-                empresas_id=empresa_id
-            )
-            session.add(nueva_cuenta)
-            session.commit()
-
-
-        return {"message": "Plan de cuentas y asientos contables creados con éxito."}
-
-    except Exception as e:
-        session.rollback()
-        raise HTTPException(status_code=500, detail=f"Error interno del servidor: {str(e)}")
-    
-
 @app.get("/empresas/{empresa_id}/planes")
 def obtener_planes(empresa_id: int):
     plan_cuentas = session.query(PlanCuentas).filter(PlanCuentas.id_empresas == empresa_id).all()
